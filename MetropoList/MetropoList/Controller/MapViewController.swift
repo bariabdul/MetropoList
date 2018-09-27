4215228f402a9d564f8cf7d41818f2aca9df42ee
bariabdul <abdul.bari.1@outlook.com>
2018-09-26 20:24:08 -0400

    Added the json file and parsed it

---
+++ MetropoList/MetropoList/Controller/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  MetropoList
+//
+//  Created by Bari Abdul on 9/26/18.
+//  Copyright © 2018 Bari Abdul. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
