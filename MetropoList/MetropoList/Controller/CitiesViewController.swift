73a4e1cd75b906c6b6a64d03e99f55cadad258a5
bariabdul <abdul.bari.1@outlook.com>
2018-09-27 03:55:54 -0400

    Some more code cleanup and documentation

--- MetropoList/MetropoList/Controller/CitiesViewController.swift
+++ MetropoList/MetropoList/Controller/CitiesViewController.swift
@@ -8,39 +8,54 @@
 
 import UIKit
 
-class CitiesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource /*UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate*/ {
+
+/*
+ 
+ This View Controller file is responsible to hold delegates for the search bar, table view. it helps in displaying the labels in the table view by calling the table view cells sublass method update views and reusing the identifier to make it efficient. I prefer using arrays in this case throughout the application since I find it easier to perform operations based on needs, for example filtering or sorting.
+ 
+ I made sure that I follow MVC design pattern to separate out the logic from the UI that way code is much cleaner and much more understandable.
+ 
+ */
+
+class CitiesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    //outlet for the table view
     @IBOutlet weak var tableView: UITableView!
 
+    //maintain an array other than the cities array in order to have filtered results as per user search
     var filteredCities = [Cities]()
+    //initializing the search controller within the same view
     let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //set up a few parameters here for the search controller
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.placeholder = "Search cities"
-        navigationItem.searchController = searchController
+        if #available(iOS 11.0, *) {
+            navigationItem.searchController = searchController
+        } else {
+            // Fallback on earlier versions
+        }
         definesPresentationContext = true
         
+        //set the table views delegate and data source to this view controller
         tableView.delegate = self
         tableView.dataSource = self
+
         self.tableView.rowHeight = 75
         
-        CityService.instance.fetchCities()//{ (success) in
-        //UserDetailService.instance.userDetails.sort(by: { $0.userTitle > $1.userTitle })
+        
+        //After fetching the cities, as per the requirement I have sorted the array based on the name of the city and then reloaded the table view
+        CityService.instance.fetchCities()
         CityService.instance.cities.sort(by: { $0.cityName < $1.cityName })
             self.tableView.reloadData()
-        //}
-//        if text.contains("T") {
-//            print("true")
-//        } else {
-//            print("false")
-//        }
     }
     
+    //this table view data source method will tell the table view to have a certain number of rows according to the original list or the filtered list while performing the search
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isFiltering() {
             return filteredCities.count
@@ -48,6 +63,7 @@ class CitiesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return CityService.instance.getCities().count
     }
     
+    //this method will ask the data source which is this view controller for a cell to insert in a particular location
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell") as? CityListCell {
             let city: Cities
@@ -56,7 +72,7 @@ class CitiesViewController: UIViewController, UITableViewDelegate, UITableViewDa
             } else {
                 city = CityService.instance.getCities()[indexPath.row]
             }
-            //let city = CityService.instance.getCities()[indexPath.row]
+            //after sorting out if its the filtered list or the original list to be dislpayed, call the updateViews function to render the cells.
             cell.updateViews(city: city)
             return cell
         } else {
@@ -64,26 +80,25 @@ class CitiesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
+    //it will tell the delegate which row has been selected
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //MapService.instance.setID(id: CityService.instance.getCities()[indexPath.row].cityId)
-//        if searchController.searchBar.text == "" {
-//            MapService.instance.setLocationData(city: CityService.instance.getCities()[indexPath.row])
-//        } else {
-//            MapService.instance.setLocationData(city: filteredCities[indexPath.row])
-//        }
-        
+        //based on the results displayed on the screen, pass the right indexPath.row to see the correct output in the next screen
         if !isFiltering() {
-                        MapService.instance.setLocationData(city: CityService.instance.getCities()[indexPath.row])
-                    } else {
-                        MapService.instance.setLocationData(city: filteredCities[indexPath.row])
-                    }
+            MapService.instance.setLocationData(city: CityService.instance.getCities()[indexPath.row])
+        } else {
+            MapService.instance.setLocationData(city: filteredCities[indexPath.row])
+        }
+        
+        //after the index is selected, perform the segue
         performSegue(withIdentifier: "toMapView", sender: indexPath)
     }
     
+    //this function will return true if the search bar is empty or nil
     func searchBarIsEmpty() -> Bool {
         return searchController.searchBar.text?.isEmpty ?? true
     }
     
+    //this function will filter the cities based on the search text and will put the results in filtered cities array
     func filterContentsForSearchText(_ searchText: String, scope: String = "All") {
         filteredCities = CityService.instance.getCities().filter({( city: Cities) -> Bool in
             return city.cityName.lowercased().contains(searchText.lowercased())
@@ -91,12 +106,14 @@ class CitiesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         tableView.reloadData()
     }
     
+    //with this function, the table view will be able to determine when to use filtered results. it will simply check whether the user is searching 
     func isFiltering() -> Bool {
         return searchController.isActive && !searchBarIsEmpty()
     }
 }
 
 extension CitiesViewController: UISearchResultsUpdating {
+    //this method will help the view controller to respond to search bar, which will help in updating the results based on the information the user enters into the search bar
     func updateSearchResults(for searchController: UISearchController) {
         filterContentsForSearchText(searchController.searchBar.text!)
     }
--- MetropoList/MetropoList/Controller/MapViewController.swift
+++ MetropoList/MetropoList/Controller/MapViewController.swift
@@ -9,24 +9,33 @@
 import UIKit
 import MapKit
 
+//this is the view controller file associated with the second screen which is the map view and in order to achieve that, we will have to conform to MKMapViewDelegate to place annotations and the city name to that annotation.
 class MapViewController: UIViewController, MKMapViewDelegate {
 
+    //an outlet to the map view that we have in storyboard
     @IBOutlet weak var mapView: MKMapView!
     
+    //to have annotation access only from this file
     fileprivate var annotation: MKAnnotation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //first thing, get the name of the city that way you wont find yourself lost
         self.navigationItem.title = MapService.instance.getLocationData().cityName
         
+        //set the delegate to self and map type to standard. you may prefer satellite too.. or maybe hybrid.. your call..
         mapView.delegate = self
         mapView.mapType = .standard
         // Do any additional setup after loading the view.
         
+        
+        //set the properties to annotation like the coordinates and a title associated with the annotation.
         let pointAnnotation = MKPointAnnotation()
-        pointAnnotation.title = MapService.instance.getLocationData().cityName//item.1["name"].stringValue
+        pointAnnotation.title = MapService.instance.getLocationData().cityName
         pointAnnotation.coordinate = CLLocationCoordinate2DMake(MapService.instance.getLocationData().cityLatitude, MapService.instance.getLocationData().cityLongitude)
         
-        let pinAnnotationView = MKPinAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
+        //set the annotation as per the coordinates and center them on the screen
+        let pinAnnotationView = MKAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
         self.mapView.centerCoordinate = pointAnnotation.coordinate
         self.mapView.addAnnotation(pinAnnotationView.annotation!)
     }
